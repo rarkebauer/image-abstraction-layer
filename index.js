303185f21b388d3c1d9da1097efa01d3de5e4b94
Rachel Arkebauer <rarkebauer@gmail.com>
2017-06-05 20:03:42 -0400

    added .gitignore

--- index.js
+++ index.js
@@ -6,15 +6,16 @@ var port = process.env.PORT || 3000;
 var url = require('./url.js');
 
 function urlRoute(req, res) {
-	var isAUrl = url.checkUrl(req.params[0]);
-	res.json(isAUrl);
+	var search = req.params[0];
+	console.log(req.params);
+	res.json(req.params);
 }
 
 app.listen(port, function() {
 	console.log('Example app listening on port ' + 3000);
 })
 
-app.get('/new/*', urlRoute);
+app.get('/api/imagesearch/*', urlRoute);
 app.get('/', function(req, res) {
 	res.send('Welcome to the express server')
 })
